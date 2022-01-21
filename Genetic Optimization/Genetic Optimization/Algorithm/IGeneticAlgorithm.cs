6df18a58d613e09ee335295e033739a0cb4f3302
ParZZ1vaLl <72264071+ParZZ1vaLl@users.noreply.github.com>
2022-01-21 22:19:55 +0200

    Delete Genetic Optimization directory

--- Genetic Optimization/Genetic Optimization/Algorithm/IGeneticAlgorithm.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Genetic_Optimization
-{
-    internal interface IGeneticAlgorithm<T>
-    {
-        public LinkedList<ISolutionPhenotype<T>> Population { get; }
-        public void Iterate();
-    }
-}
